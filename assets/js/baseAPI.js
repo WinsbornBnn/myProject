6a8e256d225f117b5589296a76f8cbc0ae55544f
zhuxingping <1322748878@qq.com>
2020-07-30 15:05:14 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,10 @@
 $.ajaxPrefilter(function (options) {
-    // options.url = 'http://192.168.50.200:3007' + options.url;
+    //一定放到地址的前面，后面加了‘/**’开头，就不用再加了
+    if (options.url.startsWith('/my/')) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    // options.url = 'http://192.168.50.200:3007' + options.url;
 });
